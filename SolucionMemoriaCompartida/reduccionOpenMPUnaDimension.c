0c288f484ba3cd14070b3861b10aed12ae17be9c
Jeronimo Alfonsin <jeronimoalfonsin@gmail.com>
2022-05-18 14:12:44 -0300

    Reduccion 1 dimension open MP casi lista

--- SolucionMemoriaCompartida/reduccionOpenMPUnaDimension.c
+++ SolucionMemoriaCompartida/reduccionOpenMPUnaDimension.c
@@ -5,6 +5,7 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <omp.h>
 
 #define PRESICION 0.01
 
@@ -12,7 +13,7 @@
 int DIM = 512;
 float *A, *B;
 
-/** Inicializa el Vector A con valores Aleatorios del 1 al 100
+/** Inicializa el Vector A con valores Aleatorios del 0 a 1
  * @param A Vector a inicializar
  */
 void init_vector_ceros_y_unos(float *A)
@@ -48,9 +49,17 @@ double dwalltime()
 
 int main(int argc, char *argv[])
 {
-
     // Variable auxiliar para calculo del tiempo
     double timetick;
+    int cantIteraciones = 0;
+    int numThreads = 4;
+
+    if (argc > 1) {
+        DIM = atoi(argv[1]);
+    }
+    if (argc > 2) {
+        numThreads = atoi(argv[2]);
+    }
 
     // Aloca memoria para el vector principal y el Vector Resultado
     A = (float *)malloc(sizeof(float) * DIM);
@@ -58,44 +67,48 @@ int main(int argc, char *argv[])
 
     init_vector_ceros_y_unos(A);
 
+    /*
     printf("Vector A: \n");
     print_vector(A);
     printf("\n");
-
+    */
     bool convergencia;
+    omp_set_num_threads(numThreads);
 
     /* Inicio de la medicion de tiempo */
     timetick = dwalltime();
 
     do
     {
+        int i;
         /** Parte I - Calculo de Vector Reducido */
-
-        for (int i = 1; i < DIM - 1; i++)
+        #pragma omp parallel for private(i) shared(A,B)
+        for (int i = 0; i < DIM; i++)
         {
-            B[i] = (A[i - 1] + A[i] + A[i + 1]) / 3;
+            if (i = 0) {
+                B[i] = (A[i] + A[i+1]) / 2;
+            } else if (i = DIM - 1) {
+                B[ DIM - 1] = (A[DIM - 1] + A[DIM - 2]) / 2;
+            } else {
+                B[i] = (A[i - 1] + A[i] + A[i + 1]) / 3;
+            }
         }
 
-        // Casos extremos
-        B[0] = (A[0] + A[1]) / 2;
-        B[DIM] = (A[DIM - 1] + A[DIM]) / 2;
-
+        
         /** Parte II - Verificacion de Convergencia. */
 
         convergencia = true;
-
-        for (int i = 0; i < DIM; i++)
+        
+        #pragma omp parallel for reduction(&&:convergencia)
+        for (i = 0; i < DIM; i++)
         {
-            if (fabs(B[0] - B[i]) > PRESICION)
-            {
-                convergencia = false;
-            }
+            convergencia = convergencia && !(fabs(B[0] - B[i]) > PRESICION); 
         }
 
         if (!convergencia)
         {
-            printf("El resultado no converge | Recalculando...\n");
-
+            //printf("El resultado no converge | Recalculando...\n");
+            cantIteraciones ++;
             // Copio todo el Vector B en A, y vuelvo a utilizar B como auxiliar
             for (int i = 0; i < DIM; i++)
             {
@@ -107,6 +120,7 @@ int main(int argc, char *argv[])
 
     /* Fin de la medicion de tiempo */
     printf("Tiempo en segundos para convergencia %f\n", dwalltime() - timetick);
+    printf("Cantidad de reducciones aplicadas: %d\n", cantIteraciones);
 
     free(A);
     free(B);
