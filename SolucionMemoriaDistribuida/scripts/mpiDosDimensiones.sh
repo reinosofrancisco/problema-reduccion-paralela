0b59b0253078609081ab01c9038633c8b38bd9be
reinosofrancisco <reinosofrancisco99@gmail.com>
2022-05-29 16:45:16 -0300

    Scripts

---
+++ SolucionMemoriaDistribuida/scripts/mpiDosDimensiones.sh
@@ -0,0 +1,26 @@
+#!/bin/bash  
+# chmod +x script.sh
+
+mpicc -o dos ../mpiDosDimensionesReduccion.c 
+
+for i in 1 2 4 
+
+do
+
+echo -e "----------------------"
+echo "NUM_THREADS = $i"
+echo -e "\n"
+
+mpirun -np $i dos 256
+echo -e "\n"
+
+mpirun -np $i dos 512
+echo -e "\n"
+
+mpirun -np $i dos 1024
+echo -e "\n"
+
+# mpirun -np $i dos 2048
+# echo -e "\n"
+
+done
